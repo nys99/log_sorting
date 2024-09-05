2a09a56ddc4ed2a4f5eced6383c5bb894b2be9cc
Nikhil Sharma <nikhil@alldaykitchens.com>
2024-09-04 19:08:23 -0700

    solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,53 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
-};
+const { Heap } = require("heap-js");
+
+async function asyncSortedMerge(logSources, printer) {
+  // initiate min heap with custom comporator
+  const minHeap = new Heap(
+    (a, b) => a.log.date.getTime() - b.log.date.getTime()
+  );
+
+  // asynchronously init the min heap with first log from each source
+  await Promise.all(
+    logSources.map(async (source) => {
+      const log = await source.popAsync();
+      if (log) {
+        console.log("Adding to heap:", log.date);
+        minHeap.push({ log, source });
+      }
+    })
+  );
+
+  // process the heap
+  while (!minHeap.isEmpty()) {
+    const { log, source } = minHeap.pop();
+    printer.print(log);
+
+    const nextLog = await source.pop();
+    if (nextLog) {
+      console.log("Adding to heap:", nextLog.date);
+      minHeap.push({ log: nextLog, source });
+    }
+  }
+
+  printer.done();
+  console.log("Asynchronous log sorting complete.");
+}
+
+module.exports = asyncSortedMerge;
+
+/*
+  The time complexity of each run is broken down into the initialization and the processing.
+  The initialization takes s log (s) where s is the number of sources. Since each insert 
+  operation takes log (s) and ther are s number of sources.
+
+  The processing takes n log (s) where n is the number of logs. Since for each log we must 
+  remove and insert into the heap which take log (s).
+
+  If we consider the the scale of processing millions of sources and logs, the I/O operations
+  of reading from the source could be a significant cost. To improve this we can batch the 
+  operations where we are pulling from the source and store it in memory. As the in memory
+  store for each source begins to decrease we could then lazy load more logs from the source.
+*/
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,37 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+const { Heap } = require("heap-js");
+
+function syncSortedMerge(logSources, printer) {
+  // initiate min heap with custom comporator
+  const minHeap = new Heap(
+    (a, b) => a.log.date.getTime() - b.log.date.getTime()
+  );
+
+  // init the min heap with first log from each source
+  logSources.forEach((source, _index) => {
+    const log = source.pop();
+    if (log) {
+      console.log("Adding to heap:", log.date);
+      minHeap.push({ log, source });
+    }
+  });
+
+  // process the heap
+  while (!minHeap.isEmpty()) {
+    const { log, source } = minHeap.pop();
+    printer.print(log);
+
+    const nextLog = source.pop();
+    if (nextLog) {
+      console.log("Adding to heap:", nextLog.date);
+      minHeap.push({ log: nextLog, source });
+    }
+  }
+
+  printer.done();
+  console.log("Synchronous log sorting complete.");
+}
+
+module.exports = syncSortedMerge;
